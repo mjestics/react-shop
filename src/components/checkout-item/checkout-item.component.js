64f047096fb035665647c8ea8baf6831d27b0e47
mjanik <mjanik2@sii.pl>
2020-05-11 11:17:10 +0200

    increasing/decreasing/removing items from checkout page

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import {addItemToCart, clearItemFromCart, removeItem} from '../../redux/cart/cart.actions';
+import {connect} from 'react-redux';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({cartItem: {name, quantity, price, imageUrl}}) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt='item' />
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>
-    </div>
-);
 
-export default CheckoutItem;
+const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
+    const {name, quantity, price, imageUrl} = cartItem;
+
+    return (<div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt='item'/>
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <div className='value'>{quantity}</div>
+                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+            </span>
+            <span className='price'>{price}</span>
+            <div onClick={() => clearItem(cartItem)} className='remove-button'>&#10005;</div>
+        </div>
+    )
+};
+
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItemToCart(item)),
+    removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
