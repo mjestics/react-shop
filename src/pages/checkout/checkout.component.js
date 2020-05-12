da3ab07246c61fc9cc130fc16ff9dbdb89817103
mjanik <mjanik2@sii.pl>
2020-05-12 12:01:30 +0200

    minor changes; adding half-dummy stripe payment button

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -5,6 +5,7 @@ import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 import {connect} from 'react-redux';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({cartItems, totalPrice}) => (
     <div className='checkout-page'>
@@ -31,6 +32,7 @@ const CheckoutPage = ({cartItems, totalPrice}) => (
             )
         }
         <div className='total'>{`total: ${totalPrice}`}</div>
+        <StripeCheckoutButton price={totalPrice}/>
     </div>
 );
 
