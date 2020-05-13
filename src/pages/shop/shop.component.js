bdac8df9b807dd2f90b74ea72a7ee6eff735e526
mjanik <mjanik2@sii.pl>
2020-05-13 14:11:05 +0200

    using react thunk for async data fetching

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -2,46 +2,41 @@ import React from 'react';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import {Route} from 'react-router-dom';
 import CollectionPage from '../collection/collection.component';
-import {convertCollectionsSnapshotToMap, firestore} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
-import {updateCollections} from '../../redux/shop/shop.actions';
+import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import {createStructuredSelector} from 'reselect';
+import {isCollectionsLoaded} from '../../redux/shop/shop.selectors';
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-    state = {
-        loading: true
-    };
-
     componentDidMount() {
-        const {updateCollections} = this.props;
-        const collectionRef = firestore.collection('collections');
-        collectionRef.onSnapshot(async snapshot => {
-            updateCollections(convertCollectionsSnapshotToMap(snapshot));
-            this.setState({loading: false});
-        });
-    };
+        this.props.fetchCollectionsStartAsync();
+    }
 
     render() {
-        const {match} = this.props;
-        const {loading} = this.state;
+        const {match, isCollectionLoaded} = this.props;
         return (<div className='shop-page'>
                 <Route exact
                        path={`${match.path}`}
-                       render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>}
+                       render={(props) => <CollectionsOverviewWithSpinner isLoading={!isCollectionLoaded} {...props}/>}
                 />
                 <Route path={`${match.path}/:collectionId`}
-                       render={(props) => <CollectionsPageWithSpinner isLoading={loading} {...props}/>}
+                       render={(props) => <CollectionsPageWithSpinner isLoading={!isCollectionLoaded} {...props}/>}
                 />
             </div>
         )
     };
 };
 
-const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+const mapStateToProps = createStructuredSelector({
+    isCollectionLoaded: isCollectionsLoaded
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,32 @@
 import {ShopActionTypes} from './shop.types';
+import {convertCollectionsSnapshotToMap, firestore} from '../../firebase/firebase.utils';
 
 export const updateCollections = (collections) => ({
     type: ShopActionTypes.UPDATE_COLLECTIONS,
     payload: collections
 });
+
+const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+});
+
+const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+        collectionRef.get().then(snapshot => {
+            const map = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(map));
+        }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
+    }
+};
